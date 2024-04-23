1e633c49904defbf935612e7c22dc3d3dd1b6af2
Siripat16 <78295537+Siripat16@users.noreply.github.com>
2024-04-23 22:02:23 +0700

    Add Security
    Add Security 23 Apr 2024

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const { xss } = require("express-xss-sanitizer");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -13,6 +19,25 @@ const app = express();
 
 //add body parser
 app.use(express.json());
+//Sanitize data
+app.use(mongoSanitize());
+//Set security headers
+app.use(helmet());
+//Prevent XSS attacks
+app.use(xss());
+
+//Rate Limiting
+const limiter = rateLimit({
+  windowsMs: 10 * 60 * 1000, //10 mins
+  max: 100,
+});
+app.use(limiter);
+
+//Prevent http param pollutions
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
 
 //Cookie parser
 app.use(cookieParser());
